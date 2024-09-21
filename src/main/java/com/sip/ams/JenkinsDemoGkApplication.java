9eb9ecbf3cc1bc7422da2aa5140be45c2410e799
AbderrahmeneRidene <abd.ridene@gmail.com>
2024-09-21 01:29:47 +0100

    first commit after set-url origine

--- src/main/java/com/sip/ams/JenkinsDemoGkApplication.java
+++ src/main/java/com/sip/ams/JenkinsDemoGkApplication.java
@@ -9,6 +9,7 @@ public class JenkinsDemoGkApplication {
 	public static void main(String[] args) {
 	
 		SpringApplication.run(JenkinsDemoGkApplication.class, args);
+		System.out.println("Hello World");
 		System.out.println("La somme de 2 + 3 = " + Calcul.somme(2,3));
 		System.out.println("Le produit de 2 * 3 = " + Calcul.produit(2,3));
 	}
